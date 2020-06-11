f794cce01ffab31e7e94ec301e40316f645421a9
jinyoung-lee <cookie178@naver.com>
2020-06-11 20:22:22 +0900

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Release on 2020/06/11.
+//  Copyright © 2020 Jinyoung-lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
